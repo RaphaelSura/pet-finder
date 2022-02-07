2fbc96d563d67f0b9d3bc3ddf8a27f56ce5174e8
RaphaelSura <daveau.raphael@gmail.com>
2022-02-07 16:08:47 +0100

    fixed bug with project abs path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils.bot import PetAppBot
+import pathlib
 
 # TODO add telegram setup instruction
 # TODO github action
@@ -7,17 +8,18 @@ from utils.bot import PetAppBot
 
 def main():
     # database file
-    db_name = "data/dyrevaernet.db"
+    project_dir = pathlib.Path(__file__).parent.resolve()
+    db_path = project_dir.joinpath("data", "dyrevaernet.db")
 
     # telegram bot credentials
-    cred_file = "etc/telegram_creds.txt"
+    cred_file = project_dir.joinpath("etc", "telegram_creds.txt")
 
     # url and filters already
     website = "https://dyrevaernet.dk"
     dog_url = "/adopter/?q=e6fec24041f04a949c3897f522576a11_652c4af8b86f4a92be420984a04edf4c#cnt"
     cat_url = "/adopter/?q=e6fec24041f04a949c3897f522576a11_f433d6f7869844e2b86ed4d89d3b05ca#cnt"
 
-    mybot = PetAppBot(website, db_name, cred_file)
+    mybot = PetAppBot(website, db_path, cred_file)
     mybot.fetch_data_on_page(dog_url, 'dog')
     mybot.fetch_data_on_page(cat_url, 'cat')
 
